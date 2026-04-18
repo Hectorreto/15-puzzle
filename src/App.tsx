1ce511d3c4249eb5cca33f25d022a7d6c7926154
Héctor de Anda <30514545+Hectorreto@users.noreply.github.com>
2026-04-18 13:39:01 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="bg-black h-screen flex items-center justify-center">
+      <div className="bg-blue-500 w-40 h-40 p-4 border-8 border-red-500"></div>
+    </div>
+  )
+}
+
+export default App;
